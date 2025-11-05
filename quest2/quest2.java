187feefe97270656733a54699af0369dbec01489
MisqaJap <ofushgaming@gmail.com>
2025-11-05 10:56:18 +0700

    Deklarasi Variable ke 2, sebelumnya lupa di save dulu

--- quest2/quest2.java
+++ quest2/quest2.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class quest2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        double strukturData, pemrogramanBerorientasiObjek, basisData, ADP, RPL;
+        String predikatNilai;
     }
 }
 
