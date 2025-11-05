7926fe8bad3379f0697d3f4d2b7c1bf3c5e94f1c
MisqaJap <ofushgaming@gmail.com>
2025-11-05 11:03:28 +0700

    membuat user input

--- quest2/quest2.java
+++ quest2/quest2.java
@@ -2,9 +2,24 @@ import java.util.Scanner;
 
 public class quest2 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        double strukturData, pemrogramanBerorientasiObjek, basisData, ADP, RPL;
+        Scanner ns = new Scanner(System.in);
+        double strukturData, pemrogramanBerorientasiObjek, basisData, adp, rpl;
         String predikatNilai;
+
+        System.out.println("Welcome to system program seleksi asisten dosen");
+        System.out.println("--------------------------------------------------");
+        System.out.println("Silakan masukkan nilai (0-100) untuk 5 mata kuliah:");
+        System.out.print("Masukkan nilai Struktur Data: ");
+        strukturData = ns.nextDouble();
+        System.out.print("Masukkan nilai Pemrograman Berorientasi Objek: ");
+        pemrogramanBerorientasiObjek = ns.nextDouble();
+        System.out.print("Masukkan nilai Basis Data: ");
+        basisData = ns.nextDouble();
+        System.out.print("Masukkan nilai Algoritma dan Pemrograman: ");
+        adp = ns.nextDouble();
+        System.out.print("Masukkan nilai Rekayasa Perangkat Lunak: ");
+        rpl = ns.nextDouble();
+
     }
 }
 
